62485518735a22b2d04ccd562b1783b08a96ad39
Riddhivd5 <90703322+Riddhivd5@users.noreply.github.com>
2025-12-29 10:20:56 +0530

    Add files via upload

---
+++ Programs/led_eye_blink/led_eye_blink.ino
@@ -0,0 +1,40 @@
+const int ledPin = 2; 
+
+const int sensorPin = 34; 
+
+int triggerLevel = 1925; // change according to your threshold
+
+int ledState = LOW;
+
+unsigned long lastBlinkTime = 0;   
+unsigned long blinkCooldown = 500; 
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
+  Serial.begin(115200); 
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);
+  unsigned long currentTime = millis();
+
+  Serial.print(sensorValue);    
+  Serial.print(" ");         
+  Serial.println(triggerLevel); 
+
+  if (currentTime - lastBlinkTime > blinkCooldown) {
+    
+    if (sensorValue > triggerLevel) {
+
+      Serial.println("BLINK DETECTED!");
+
+      ledState = !ledState;
+      digitalWrite(ledPin, ledState);
+     
+      lastBlinkTime = currentTime;
+    }
+  } 
+  
+  delay(10); 
+} 
